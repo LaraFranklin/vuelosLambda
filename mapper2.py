b7ed7bf7e838bced9826e24146741d23fe850491
LaraFranklin <larafranklin95@gmail.com>
2019-02-11 22:59:04 -0500

    mapper2 para los tipos de aviones

---
+++ mapper2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# Use the sys module
+import sys
+
+# 'file' in this case is STDIN
+def read_input(file):
+    # Split each line into words
+    for line in file:
+        yield line.split(',')
+
+def main(separator='\t'):
+    # Read the data using read_input
+    data = read_input(sys.stdin)
+    #data = read_input(open('xaa.csv','r'))
+    # Process each word returned from read_input
+    datos = []
+    
+    for words in data:
+        #datos.append(words)
+        #print(datos)
+        # Process each line
+        if (words[15]!="DepDelay" and words[15] != "NA" and int(words[15])>0):
+            key = words[10]
+            valor = int(words[15])
+            print '%s%s%d' % (key, separator, valor)
+if __name__ == "__main__":
+    main()
